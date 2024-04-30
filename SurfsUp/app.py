b6f259e450c2bc2588da8e52c47b7cf0c3374cf8
ECPrice1 <ericprice@Erics-MacBook-Pro.local>
2024-04-29 18:24:24 -0600

    updates to climate.ipynb and app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -37,6 +37,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
+    
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
@@ -63,31 +64,37 @@ def precipitation():
 
     session.close()
 
-    recent_measurements = []
-    for prcp, date in results:
-        measurement_dict = {}
-        measurement_dict["name"] = name
-        measurement_dict["age"] = age
+    #recent_measurements = []
+    #for prcp, date in results:
+        #measurement_dict = {}
+        #measurement_dict["name"] = name
+        #measurement_dict["age"] = age
         
-        measurements.append(recent_measurements)
+        #measurements.append(recent_measurements)
 
-    return jsonify(measurements)
+    #return jsonify(measurements)
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    #all_names = list(np.ravel(results))
 
-    return jsonify(all_names)
+    #return jsonify(all_names)
 
 #@app.route("/api/v1.0/stations")
-session.query(func.count(Station.station)).all()
+#session.query(func.count(Station.station)).all()
 
-session.query(Measurement.station, func.count(Measurement.station)).\
-    group_by(Measurement.station).\
-    order_by(func.count(Measurement.station).desc()).all()
+#session.query(Measurement.station, func.count(Measurement.station)).\
+    #group_by(Measurement.station).\
+    #order_by(func.count(Measurement.station).desc()).all()
 
-#@app.route("/api/v1.0/tobs")
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-temperatures = session.query(Measurement.tobs, func.count(Measurement.tobs)).\
+    date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    query_date = date - dt.timedelta(days=365)
+
+    temperatures = session.query(Measurement.tobs, func.count(Measurement.tobs)).\
     filter(Measurement.station == 'USC00519281').\
     filter(Measurement.date > query_date).\
     group_by(Measurement.tobs).all()
